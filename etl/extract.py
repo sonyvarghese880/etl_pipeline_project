8dc3a9451e358d4b15064c8e44f1601080ee78f5
sony <sonyvarghese880@gmail.com>
2025-06-06 19:56:19 -0400

    Updated extract.py to accomodate filepath

--- etl/extract.py
+++ etl/extract.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def extract_data(filepath):
-    """Read Parquet file and return a DataFrame."""
+def extract_data(file_path='data/yellow_tripdata_2025-01.parquet'):
+    """Read Parquet file and return a DataFrame."""  
+    print(f"Reading data from {file_path} ...")                 
     data = pd.read_parquet(file_path)
     return data
